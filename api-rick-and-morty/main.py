07802f7f19af6c3c83509fa45ddfcaba453dec9d
Beatris Cicotosto <biacicotosto@gmail.com>
2024-05-17 10:54:40 -0300

    api rick and morty

---
+++ api-rick-and-morty/main.py
@@ -0,0 +1,17 @@
+import requests
+
+def fetch_data(endpoint, filters={}):
+    url = f"https://rickandmortyapi.com/api/{endpoint}"
+    response = requests.get(url, params=filters)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+characters = fetch_data("character", {"nome": "Rick"})
+
+if characters:
+    print(characters)
+else:
+    print("Failed to fetch data")
